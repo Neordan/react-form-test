bebccee0658e15e4e8ee578c5e8f67cd8fa087b1
Neordan <83787849+Neordan@users.noreply.github.com>
2023-05-26 23:45:01 +0200

    Suppression de fichiers dans public

---
+++ src/FormTemplate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormTemplate = () => {
+    return (
+        <div>
+            <h1>Formulaire de contact</h1>
+        </div>
+    );
+};
+
+export default FormTemplate;
