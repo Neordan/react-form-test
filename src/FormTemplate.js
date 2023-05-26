c19ccbdd8936f1259d2a1b7b1c1784f026fbb69c
Neordan <83787849+Neordan@users.noreply.github.com>
2023-05-27 00:41:06 +0200

    Récupération d'un exemple de formulaire dans la doc de Emailjs
    Ajout de mes ID

--- src/FormTemplate.js
+++ src/FormTemplate.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const FormTemplate = () => {
-    return (
-        <div>
-            <h1>Formulaire de contact</h1>
-        </div>
-    );
-};
+  const form = useRef();
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_4nd5qvo', 'template_pqn415f', form.current, 'z0M5yTmtGUHErJ_2E')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
 export default FormTemplate;
