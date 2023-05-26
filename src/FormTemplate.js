2f194717ce56596af793c7e6074bc1c39a8678bf
Neordan <83787849+Neordan@users.noreply.github.com>
2023-05-27 00:43:20 +0200

    Changemnt des "name" dans les inputs en relation avec Emailjs

--- src/FormTemplate.js
+++ src/FormTemplate.js
@@ -18,12 +18,12 @@ const FormTemplate = () => {
   return (
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
-      <input type="text" name="user_name" />
+      <input type="text" name="name" />
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="email" />
       <label>Message</label>
       <textarea name="message" />
-      <input type="submit" value="Send" />
+      <input type="submit" value="Envoyer" />
     </form>
   );
 };
