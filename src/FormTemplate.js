f881bc181641d64442742340153c6a72fe5eb044
Neordan <83787849+Neordan@users.noreply.github.com>
2023-05-27 01:40:17 +0200

    Ajout de la div parent form-container et celle pour l'affichage du message d'envoie ou non

--- src/FormTemplate.js
+++ src/FormTemplate.js
@@ -2,36 +2,39 @@ import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
 const FormTemplate = () => {
-  // Crée une référence au formulaire
-  const form = useRef();
+    // Crée une référence au formulaire
+    const form = useRef();
 
-  // Fonction pour envoyer l'email
-  const sendEmail = (e) => {
-    e.preventDefault();
+    // Fonction pour envoyer l'email
+    const sendEmail = (e) => {
+        e.preventDefault();
 
-    // Envoie le formulaire via EmailJS en utilisant la référence du formulaire
-    emailjs
-      .sendForm('service_jiwg0tn', 'template_pqn415f', form.current, 'z0M5yTmtGUHErJ_2E')
-      .then((result) => {
-        console.log(result.text);
-        // remise à 0 du formulaire
-        form.current.reset();
-      }, (error) => {
-        console.log(error.text);
-      });
-  };
+        // Envoie le formulaire via EmailJS en utilisant la référence du formulaire
+        emailjs
+            .sendForm('service_jiwg0tn', 'template_pqn415f', form.current, 'z0M5yTmtGUHErJ_2E')
+            .then((result) => {
+                console.log(result.text);
+                // remise à 0 du formulaire
+                form.current.reset();
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
 
-  return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="name" required autoComplete="off" />
-      <label>Email</label>
-      <input type="email" name="email" required autoComplete="off" />
-      <label>Message</label>
-      <textarea name="message" required />
-      <input type="submit" value="Envoyer" />
-    </form>
-  );
+    return (
+        <div className="form-container">
+            <form ref={form} onSubmit={sendEmail}>
+                <label>Name</label>
+                <input type="text" name="name" required autoComplete="off" />
+                <label>Email</label>
+                <input type="email" name="email" required autoComplete="off" />
+                <label>Message</label>
+                <textarea name="message" required />
+                <input type="submit" value="Envoyer" />
+            </form>
+            <div className="form-message"></div>
+        </div>
+    );
 };
 
 export default FormTemplate;
